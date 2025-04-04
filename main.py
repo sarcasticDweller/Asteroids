9504e3e19ca9625c04e186bbf2c9f1edcdded81f
sarcasticDweller <capslocked.strife@gmail.com>
2025-04-04 17:58:00 -0400

    Boilerplate

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
+#!venv/bin/python3
+# python libraries
 import pygame
+# project libraries
+from constants import *
+
+def io():
+    pass
+
+def update():
+    pass
+
+def draw(screen):
+    screen.fill((0, 0, 0))
+    screen.flip()
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        # make the window's close button work
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        screen.flip()
+
 
 if __name__ == "__main__":
     main()
